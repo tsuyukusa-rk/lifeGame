8b8345390ffdbda0c81f990fe61b8ef6bd77fda0
lc-kondo <kondo@lightcafe.co.jp>
2017-05-23 14:04:20 +0900

    とりあえず下地だけ

---
+++ js/index.js
@@ -0,0 +1,21 @@
+onload = function() {
+  draw();
+};
+function draw() {
+  /* canvas要素のノードオブジェクト */
+  var canvas = document.getElementById('canvassample');
+  /* canvas要素の存在チェックとCanvas未対応ブラウザの対処 */
+  if ( ! canvas || ! canvas.getContext ) {
+    return false;
+  }
+  /* 2Dコンテキスト */
+  var ctx = canvas.getContext('2d');
+  /* 四角を描く */
+  ctx.beginPath();
+  ctx.moveTo(20, 20);
+  ctx.lineTo(120, 20);
+  ctx.lineTo(120, 120);
+  ctx.lineTo(20, 120);
+  ctx.closePath();
+  ctx.stroke();
+}
